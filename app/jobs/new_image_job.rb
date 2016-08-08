3e509b94d7e718757cd457e2c406225733920544
Prokop Simek <prokop.simek@applifting.cz>
2016-08-08 19:22:01 +0200

    test

--- app/jobs/new_image_job.rb
+++ app/jobs/new_image_job.rb
@@ -3,13 +3,13 @@ class NewImageJob # This is a Sidekiq job, not the normal ActiveJob, beacuse of 
   def perform
     Rails.logger.info '===== Img refreshing ======'
 
-    all_numbers = (1..ENV['MAX_IMAGE_NAME'].to_i).to_a
-    all_numbers = all_numbers - Image.all.map{ |i| i.name.to_i }
-    random_name = all_numbers.sample
-
-    if random_name.present? && DateTime.now >= DateTime.civil(2016, 8, 1)
-      Image.create!(date: Date.today, name: random_name)
-    end
+    # all_numbers = (1..ENV['MAX_IMAGE_NAME'].to_i).to_a
+    # all_numbers = all_numbers - Image.all.map{ |i| i.name.to_i }
+    # random_name = all_numbers.sample
+    #
+    # if random_name.present? && DateTime.now >= DateTime.civil(2016, 8, 1)
+    #   Image.create!(date: Date.today, name: random_name)
+    # end
 
     Rails.logger.info "===== Img refreshed ======"
   end
